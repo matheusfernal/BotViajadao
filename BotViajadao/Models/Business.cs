0398934c358c39074e94b5e178d5d33ea18cac2c
Matheus Andrade Fernal <matheusfernal@gmail.com>
2018-03-09 23:41:40 -0300

    Complementando a resposta recebida do Yelp

--- BotViajadao/Models/Business.cs
+++ BotViajadao/Models/Business.cs
@@ -5,9 +5,25 @@ namespace BotViajadao.Models
     public class Business
     {
         public string Id { get; set; }
+
         public string Name { get; set; }
 
         [JsonProperty("image_url")]
         public string ImageUrl { get; set; }
+
+        public string Url { get; set; }
+
+        [JsonProperty("review_count")]
+        public int ReviewCount { get; set; }
+
+        public float Rating { get; set; }
+
+        [JsonProperty("display_phone")]
+        public string DisplayPhone { get; set; }
+
+        public Localizacao Location { get; set; }
+
+        public Coordenadas Coordinates { get; set; }
+
     }
 }
---
+++ BotViajadao/Models/Localizacao.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BotViajadao.Models
+{
+    public class Localizacao
+    {
+        [JsonProperty("display_address")]
+        public string[] DisplayAdress { get; set; }
+    }
+}
