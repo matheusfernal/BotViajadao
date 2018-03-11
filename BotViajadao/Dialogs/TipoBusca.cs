8ef403ddf8117f3ef0534d5563cb23f419a1ce27
Matheus Andrade Fernal <matheusfernal@gmail.com>
2018-03-10 22:31:31 -0300

    Adicionando arquivo TipoBusca.cs que faltou

---
+++ BotViajadao/Dialogs/TipoBusca.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace BotViajadao.Dialogs
+{
+    public enum EnumTipoBusca
+    {
+        Hotel,
+        Restaurante,
+        Passeio
+    }
+
+    public static class TipoBusca
+    {
+        public static string MensagemPesquisandoItens(EnumTipoBusca tipoBusca)
+        {
+            switch (tipoBusca)
+            {
+                case EnumTipoBusca.Hotel:
+                    return "Estou pesquisando hoteis para vc...";
+                case EnumTipoBusca.Restaurante:
+                    return "Segura a fome que já estou encontrando restaurantes para vc.";
+                case EnumTipoBusca.Passeio:
+                    return "Já estou consultando minhas fontes sobre passeios legais.";
+                default:
+                    return "Relaxa que já vou encontrar o que procura.";
+            }
+        }
+
+        public static string MensagemItensEncontrados(EnumTipoBusca tipoBusca, int quantidadeItens)
+        {
+            switch (tipoBusca)
+            {
+                case EnumTipoBusca.Hotel:
+                    return $"Aqui estão {quantidadeItens} hoteis que encontrei";
+                case EnumTipoBusca.Restaurante:
+                    return $"Encontrei {quantidadeItens} restaurantes pra vc. Espero que algum seja delicioso.";
+                case EnumTipoBusca.Passeio:
+                    return $"Esses {quantidadeItens} passeios que encontrei parecem legais.";
+                default:
+                    return "Relaxa que já vou encontrar o que procura.";
+            }
+        }
+    }
+}
